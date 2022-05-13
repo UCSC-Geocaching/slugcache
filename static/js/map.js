9c8c81b7267c2fb82d591fd88ccb763b3c6d38a5
Christopher Sterza <ChristopherSterza@gmail.com>
2022-05-12 22:59:50 -0700

    Widened the bounded section of the map to cover more of the north woods

--- static/js/map.js
+++ static/js/map.js
@@ -1,130 +1,129 @@
 mapboxgl.accessToken =
-'pk.eyJ1IjoiY3N0ZXJ6YSIsImEiOiJjbDF0dDRleG0yMWpkM2Ztb3B0YWZoaTR6In0.09vTcRrP3ty1lWFuouDsiw'; //store in environ var.
+  'pk.eyJ1IjoiY3N0ZXJ6YSIsImEiOiJjbDF0dDRleG0yMWpkM2Ztb3B0YWZoaTR6In0.09vTcRrP3ty1lWFuouDsiw'; //store in environ var.
 // ##################################
 
 // This will be the object that will contain the Vue attributes
 // and be used to initialize it.
 let app = {};
 
-
 // Given an empty app object, initializes it filling its attributes,
 // creates a Vue instance, and then initializes the Vue instance.
 let init = (app) => {
+  // This is the Vue data.
+  app.data = {
+    // Complete as you see fit.
+  };
 
-    // This is the Vue data.
-    app.data = {
-        // Complete as you see fit.
-    };
-
-    app.enumerate = (a) => {
-        // This adds an _idx field to each element of the array.
-        let k = 0;
-        a.map((e) => {e._idx = k++;});
-        return a;
-    };
-      
-    /**
-    * Sets GPS position to coordinates pointed to by position
-    */
-    app.successLocation = function(position){
-        setupMap([position.coords.longitude, position.coords.latitude])
-    }
-    
-    /**
-    * Error handler, sets GPS location to a default SC coordinate
-    */
-    app.errorLocation = function(){
-        setupMap([-122.05820423052975, 36.98810503659569])
-    }
-
-    /**
-    * Adds marker for current location, begins tracking
-    * Will only reutrn current location if inside UCSC bounds
-    */
-   app.addGeoTracking = function(map ){
-        map.addControl(new mapboxgl.GeolocateControl({
-            positionOptions: {
-                enableHighAccuracy: true,
-                timeout: 5000
-            },
-        trackUserLocation: true,
-        showUserHeading: true
-        }));
-   }
-
-     /**
-    * Adds Navigation Bars
-    */
-    app.addNav = function (map) {
-        const nav = new mapboxgl.NavigationControl();
-        map.addControl(nav);
-    }
-
-    // This contains all the methods.
-    app.methods = {
-        // Complete as you see fit.
-        successLocation: app.successLocation,
-        errorLocation: app.errorLocation,
-        addNav: app.addNav,
-        addGeoTracking: app.addGeoTracking,
-        
-    };
-
-    // This creates the Vue instance.
-    app.vue = new Vue({
-        el: "#vue-target",
-        data: app.data,
-        methods: app.methods
+  app.enumerate = (a) => {
+    // This adds an _idx field to each element of the array.
+    let k = 0;
+    a.map((e) => {
+      e._idx = k++;
     });
+    return a;
+  };
 
-    // And this initializes it.
-    app.init = () => {
-        // Put here any initialization code.
-        // Typically this is a server GET call to load the data.
-    };
+  /**
+   * Sets GPS position to coordinates pointed to by position
+   */
+  app.successLocation = function (position) {
+    setupMap([position.coords.longitude, position.coords.latitude]);
+  };
 
+  /**
+   * Error handler, sets GPS location to a default SC coordinate
+   */
+  app.errorLocation = function () {
+    setupMap([-122.05820423052975, 36.98810503659569]);
+  };
 
-    app.setMap = function(map) {
-        app.map = map;
+  /**
+   * Adds marker for current location, begins tracking
+   * Will only reutrn current location if inside UCSC bounds
+   */
+  app.addGeoTracking = function (map) {
+    map.addControl(
+      new mapboxgl.GeolocateControl({
+        positionOptions: {
+          enableHighAccuracy: true,
+          timeout: 5000,
+        },
+        trackUserLocation: true,
+        showUserHeading: true,
+      })
+    );
+  };
 
-        app.addNav(map) //nav controls
-        app.addGeoTracking(map)
+  /**
+   * Adds Navigation Bars
+   */
+  app.addNav = function (map) {
+    const nav = new mapboxgl.NavigationControl();
+    map.addControl(nav);
+  };
 
-    };
+  // This contains all the methods.
+  app.methods = {
+    // Complete as you see fit.
+    successLocation: app.successLocation,
+    errorLocation: app.errorLocation,
+    addNav: app.addNav,
+    addGeoTracking: app.addGeoTracking,
+  };
 
-    // Call to the initializer.
-    app.init();
+  // This creates the Vue instance.
+  app.vue = new Vue({
+    el: '#vue-target',
+    data: app.data,
+    methods: app.methods,
+  });
+
+  // And this initializes it.
+  app.init = () => {
+    // Put here any initialization code.
+    // Typically this is a server GET call to load the data.
+  };
+
+  app.setMap = function (map) {
+    app.map = map;
+
+    app.addNav(map); //nav controls
+    app.addGeoTracking(map);
+  };
+
+  // Call to the initializer.
+  app.init();
 };
 
 // This takes the (empty) app object, and initializes it,
 // putting all the code i
 init(app);
 
+/**
+ * Gets current position to start map initializer
+ */
+navigator.geolocation.getCurrentPosition(
+  app.successLocation,
+  app.errorLocation,
+  { enableHighAccuracy: true }
+);
 
- /**
-    * Gets current position to start map initializer
-*/
-navigator.geolocation.getCurrentPosition(app.successLocation, app.errorLocation,
-    { enableHighAccuracy: true
-})
-
- /**
+/**
  * Map Initialization
  */
- function setupMap(center) {
-    const map = new mapboxgl.Map({
-        container: 'map',
-        style: 'mapbox://styles/mapbox/outdoors-v11',
-        center: center,
-        zoom: 12,
-        maxBounds: [ 
-            [-122.08012683329952, 36.9750849217556], // Southwest coordinates 
-            [-122.0349268336223, 37.00766046433793] // Northeast coordinates 
-        ]
-        
-    })
+function setupMap(center) {
+  const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/outdoors-v11',
+    center: center,
+    zoom: 12,
+    maxBounds: [
+      [-122.080126, 36.975084], // Southwest coordinates
+      [-122.034926, 37.019977], // Northeast coordinates
+    ],
+  });
 
-    app.setMap(map);
+  app.setMap(map);
 }
 
 /*Next tasks:
