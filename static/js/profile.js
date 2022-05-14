dfab906f7e690612990d959d2e435976aade2f7d
Christopher Sterza <ChristopherSterza@gmail.com>
2022-05-14 13:37:00 -0700

    Made some CSS changes

---
+++ static/js/profile.js
@@ -0,0 +1,46 @@
+// This will be the object that will contain the Vue attributes
+// and be used to initialize it.
+let page = {};
+
+// Given an empty page object, initializes it filling its attributes,
+// creates a Vue instance, and then initializes the Vue instance.
+let init = (page) => {
+  // This is the Vue data.
+  page.data = {
+    // Complete as you see fit.
+  };
+
+  page.enumerate = (a) => {
+    // This adds an _idx field to each element of the array.
+    let k = 0;
+    a.map((e) => {
+      e._idx = k++;
+    });
+    return a;
+  };
+
+  // This contains all the methods.
+  page.methods = {
+    // Complete as you see fit.
+  };
+
+  // This creates the Vue instance.
+  page.vue = new Vue({
+    el: '#vue-target',
+    data: page.data,
+    methods: page.methods,
+  });
+
+  // And this initializes it.
+  page.init = () => {
+    // Put here any initialization code.
+    // Typically this is a server GET call to load the data.
+  };
+
+  // Call to the initializer.
+  page.init();
+};
+
+// This takes the (empty) page object, and initializes it,
+// putting all the code i
+init(page);
